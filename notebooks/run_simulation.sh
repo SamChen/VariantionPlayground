566c836321fc0e76dceb1ae41a1af94349c050be
SamChen <samjat2012cl@gmail.com>
2024-12-26 16:13:38 -0500

    update

---
+++ notebooks/run_simulation.sh
@@ -0,0 +1,2 @@
+python3.11 ./crossover_lme_fixed_within_subj_var_mp_ptau217.py --output-folder "out_seed" --num-trials 20
+python3.11 ./parallel_lme_fixed_within_subj_var_mp_ptau217.py  --output-folder "out_paral" --num-trials 20
